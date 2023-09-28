c387f4e0fb658debf025d6dca1f8a3c0011470fa
ogulcan <oulcantoprak@yahoo.com>
2023-09-27 20:04:38 -0400

    GitProjectSrc

---
+++ src/Utilis/Util.java
@@ -0,0 +1,15 @@
+package Utilis;
+
+public class Util {
+
+	public static void Zagi() {
+
+		for (int i = 0; i < 6; i++) {
+
+			System.out.println("Tankus alfonzo sikiom muhahaha!");
+
+		}
+
+	}
+
+}
---
+++ src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import Utilis.Util;
+
+public class main extends Util {
+// Run this code and method in Utils and git push to me 
+	public static void main(String[] args) {
+
+		Zagi();
+
+	}
+
+}
